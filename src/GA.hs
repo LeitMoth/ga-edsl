e2a35bc8716119471d59f87f075de66d37d2cd3c
LeitMoth <col.phillips2004@gmail.com>
2025-05-08 03:24:20 +0000

    update

--- src/GA.hs
+++ src/GA.hs
@@ -75,6 +75,11 @@ data M3 a b where
   Sum2 :: [M3 a b] -> M3 a b
   deriving (Show)
 
+funmap :: (M3 a b -> M3 a b) -> M3 a b -> M3 a b
+funmap f (Base b) = Base b
+funmap f (Prod s ms) = Prod s (map f ms)
+funmap f (Sum2 ms) = Sum2 (map f ms)
+
 data Basis = X | Y | Z
   deriving (Show)
 
@@ -92,20 +97,59 @@ broil = \case
 
 bake :: Num a => M3 a b -> M3 a b
 bake = \case
-  Prod a (b : bs) -> bake $ Prod (a*b) (map bake bs ++ map bake cs)
   Prod a ((Prod b bs) : cs) -> bake $ Prod (a*b) (map bake bs ++ map bake cs)
   Sum2 ((Sum2 bs) : cs) -> bake $ Sum2 (bs ++ cs)
   a -> a
 
+normalize :: Num a => M3 a b -> M3 a b
+normalize = pullProd
+
 pullProd :: Num a => M3 a b -> M3 a b
 pullProd = \case
-  Prod s ms -> unpack $ foldr f (s,[]) ms 
-  where
-    f a (scal,list) = let a' = pullProd a in case a' of
-      Prod scal2 list2 -> (scal*scal2, list2 ++ list)
-      asdf -> (scal, asdf : list)
-    unpack (scal,list) = Prod scal list
+  Prod s ms -> foldr f (Prod s []) ms
+    where
+      f a (Prod s1 ms1) = case pullProd a of
+        Prod s2 ms2 -> Prod (s1*s2) (ms2 ++ ms1)
+        m -> Prod s1 (m : ms1)
+      f _ _ = error "BUG, should always be a Prod"
+  Sum2 ms -> funmap pullProd (pullSum (Sum2 ms))
+  a -> a
 
+pullSum :: Num a => M3 a b -> M3 a b
+pullSum = \case
+  Sum2 ms -> foldr f (Sum2 []) ms
+    where
+      f a (Sum2 ms1) = case a of
+        Sum2 ms2 -> Sum2 (ms2 ++ ms1)
+        m -> Sum2 (m : ms1)
+      f _ _ = error "BUG, should always be a Sum2"
+  a -> a
+
+distribute :: Num a => M3 a b -> M3 a b
+distribute = \case
+  Base b -> Base b
+  Sum2 ms -> Sum2 ms
+  Prod s ms -> foldr f (Prod 1 [Sum2 []]) ms
+    where
+      f a (Prod _ [Sum2 ms1]) = case a of
+        Sum2 ms2 -> Sum2 (ms2 ++ ms1)
+        m -> Sum2 (m : ms1)
+      f _ _ = error "BUG, should always be a Sum2"
+
+distributeFromRight :: Num a => [M3 a b] -> M3 a b -> M3 a b
+distributeFromRight ms p = Sum2 $ map (\x -> Prod 1 [x,p]) ms
+
+distributeFromLeft :: Num a => M3 a b -> [M3 a b] -> M3 a b
+distributeFromLeft p ms = Sum2 $ map (\x -> Prod 1 [p,x]) ms
+
+
+pretty :: (Show a, Num a, Eq a, Show b) => M3 a b -> String
+pretty = \case
+  Base b -> show b
+  Prod 1 ms -> foldr (\x y -> pretty x ++ y) "" ms
+  Prod s ms -> show s ++ "(" ++ foldr (\x y -> pretty x ++ y) "" ms ++ ")"
+  Sum2 [] -> ""
+  Sum2 ms -> foldr1 (\x y -> x ++ "+" ++ y) (map pretty ms)
 
 
 -- data Multivector where
